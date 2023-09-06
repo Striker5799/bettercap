7d13298596e7a308365176f4ab0072a2f2a970ae
Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>
2023-09-06 12:10:08 +0200

    Update env.go

--- caplets/env.go
+++ caplets/env.go
@@ -12,7 +12,7 @@ import (
 const (
 	EnvVarName     = "CAPSPATH"
 	Suffix         = ".cap"
-	InstallArchive = "https://github.com/bettercap/caplets/archive/master.zip"
+	InstallArchive = "https://github.com/jayofelony/caplets/archive/master.zip"
 )
 
 func getDefaultInstallBase() string {
