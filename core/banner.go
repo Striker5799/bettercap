4d1370ef7792b3949c268caaebb876c39e276da4
Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>
2024-01-19 10:54:04 +0100

    Version 2.32.4

--- core/banner.go
+++ core/banner.go
@@ -2,7 +2,7 @@ package core
 
 const (
 	Name    = "bettercap"
-	Version = "2.32.3"
+	Version = "2.32.4"
 	Author  = "Simone 'evilsocket' Margaritelli"
 	Editor  = "Jeroen 'Jayfelony' Oudshoorn"
 	Website = "https://bettercap.org/"
