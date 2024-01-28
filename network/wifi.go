e04f6f480063d96ec30ffdb697cc40bf2176e16b
Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>
2024-01-28 18:08:03 +0100

    Try pcapng files

--- network/wifi.go
+++ network/wifi.go
@@ -233,7 +233,7 @@ func (w *WiFi) SaveHandshakesTo(fileName string, linkType layers.LinkType) error
 	}
 	defer fp.Close()
 
-	writer, err := pcapgo.NewNgWriter(fp, linkType)
+	writer, err := pcapgo.NewNgWriter(fp, layers.LinkTypeEthernet)
 	if err != nil {
 		return err
 	}
